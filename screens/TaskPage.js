4c0df2a36efdc19d1c9503b32eaa07b45daffce6
Mansea <6210742034@student.tu.ac.th>
2022-01-16 01:36:56 +0700

    login & register complete

---
+++ screens/TaskPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+  } from 'react-native';
+  
+
+export default Task = () => {
+    return (
+      <SafeAreaView>
+        <Text>Homepage</Text>
+      </SafeAreaView>
+    );
+  };
