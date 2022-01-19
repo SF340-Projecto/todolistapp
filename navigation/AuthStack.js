aa71f18d5e24075dfeae12c03e55d53d22e9d3c0
macchayaphol <55339550+macchayaphol@users.noreply.github.com>
2022-01-19 14:21:20 +0700

    LOGIN UI

--- navigation/AuthStack.js
+++ navigation/AuthStack.js
@@ -11,7 +11,7 @@ const AuthStack = () => {
     return (
     
          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen component={LoginScreen} name="Login" />
+            <Stack.Screen component={LoginScreen} name="Login" options={{header: ()=> null}} />
             <Stack.Screen component={RegisterScreen} name="RegisterScreen" />
           </Stack.Navigator>
       );
