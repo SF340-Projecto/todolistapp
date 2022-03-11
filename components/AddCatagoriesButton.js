905c4114fdb7008b45afa3906ebd2d944c41ed04
poom-cyber <poompim083@gmail.com>
2022-03-11 19:07:58 +0700

    Fix styleSheet

--- components/AddCatagoriesButton.js
+++ components/AddCatagoriesButton.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 import {
-
   SafeAreaView,
-  StyleSheet,
   Modal,
   View,
   TextInput,
@@ -17,6 +15,7 @@ import {
 import {useContext, useEffect, useState} from 'react';
 import firestore from '@react-native-firebase/firestore';
 import {AuthContext} from '../navigation/AuthProviders';
+import styles from '../screens/component.style.js';
 
 const { width } = Dimensions.get('window');
 var value = ''
@@ -54,14 +53,6 @@ export default function AddCatagoriesButton() {
           <Text style={styles.loginButtonText}>Add Categories</Text>
         </TouchableOpacity>
       </View>
-
-
-
-
-
-
-
-
 
       <Modal
         animationType="slide"
@@ -126,14 +117,6 @@ export default function AddCatagoriesButton() {
                 <View style={{ paddingRight:10 }}></View>
 
               </View>
-
-              
-
-
-
-
-
-
             <View>
             <View style={styles.style_flex}>
                 <TouchableOpacity style={styles.addButtonL}>
@@ -155,185 +138,6 @@ export default function AddCatagoriesButton() {
           </View>
         </View>
       </Modal>
-
-
-
-
-
-
-
-
-
-
-
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  loginButton: {
-    marginVertical: 20,
-    backgroundColor: '#fff',
-    width: 200,
-    height: 100,
-    borderRadius: 20,
-    shadowColor: '#000000',
-    shadowOpacity: 5,
-    shadowRadius: 5,
-    elevation: 5,
-  },
-  loginButtonText: {
-    textAlign: 'center',
-    color: '#25ced1',
-    fontWeight: 'bold',
-    fontSize: 24,
-    marginTop: 6,
-    justifyContent: 'center',
-  },viewWrapper: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.2)',
-  },
-  modalView: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    elevation: 5,
-    transform: [{ translateX: -(width * 0.4) },
-    { translateY: -90 }],
-    height: 400,
-    width: width * 0.8,
-    backgroundColor: '#fff',
-    borderRadius: 7,
-  },
-  textInput: {
-    width: '80%',
-    borderRadius: 5,
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    borderColor: 'rgba(0, 0, 0, 0.2)',
-    borderWidth: 1,
-    marginBottom: 8,
-  },
-  bg_modal: {
-    backgroundColor: '#000000aa',
-    flex: 1
-  },
-  paper_madal: {
-    backgroundColor: '#ffffff',
-    margin: 30,
-    marginTop: 90,
-    marginBottom: 90,
-    padding: 20,
-    borderRadius: 10,
-    flex: 1
-  },
-  text_normal: {
-    fontWeight: 'bold',
-    padding: 10,
-  },
-  input: {
-    width: '90%',
-    borderWidth: 1,
-    padding: 10,
-    fontSize: 16,
-    shadowColor: "#000000",
-    shadowOpacity: 5,
-    shadowRadius: 5,
-    elevation: 2,
-    backgroundColor: '#e5f1f1',
-    borderRadius: 5,
-  },
-  addButtonL: {
-    backgroundColor: '#707070',
-    width: '45%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
-    height: 40,
-    shadowColor: "#000000",
-    shadowOpacity: 5,
-    shadowRadius: 5,
-    elevation: 5,
-    marginBottom:20,
-  },    
-  addButtonR: {
-    backgroundColor: '#25ced1',
-    width: '45%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
-    height: 40,
-    shadowColor: "#000000",
-    shadowOpacity: 5,
-    shadowRadius: 5,
-    elevation: 5,
-    marginBottom:20,
-  },
-  style_flex:{
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingTop:280,
-    padding:35
-  },
-  style_flexColor:{
-    flex: 1,
-    flexDirection: 'row',
-    padding:20,
-  },
-  addButtonText: {
-    color: '#ffffff',
-    fontWeight: 'bold',
-    fontSize: 16,
-    textAlign:'center'
-  },
-  logo: {
-    width: 30,
-    height: 30,
-    padding:10,
-  },
-  color_red:{
-    borderRadius: 50,
-    width: 40,
-    height: 40,
-    backgroundColor:'#ff0000',
-    justifyContent: 'center',
-    padding:10
-  },
-  color_blue:{
-    borderRadius: 50,
-    width: 40,
-    height: 40,
-    backgroundColor:'#00bfff',
-    justifyContent: 'center',
-
-  },
-  color_green:{
-    borderRadius: 50,
-    width: 40,
-    height: 40,
-    backgroundColor:'#008000',
-    justifyContent: 'center',
-
-  },
-  color_yellow:{
-    borderRadius: 50,
-    width: 40,
-    height: 40,
-    backgroundColor:'#ffff00',
-    justifyContent: 'center',
-
-  },
-  color_purple:{
-    borderRadius: 50,
-    width: 40,
-    height: 40,
-    backgroundColor:'#800080',
-    justifyContent: 'center',
-
-  }
-});
--- screens/TaskPage.js
+++ screens/TaskPage.js
@@ -2,20 +2,15 @@ import React, {useState} from 'react';
 import {useContext, useEffect} from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
   Modal,
   View,
-  Dimensions,
   Text,
   TouchableOpacity,
-  Alert,
   FlatList,
   Image,
 } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import {AuthContext} from '../navigation/AuthProviders';
-import {launchImageLibrary} from 'react-native-image-picker'; // Migration from 2.x.x to 3.x.x => showImagePicker API is removed.
-import storage from '@react-native-firebase/storage';
 import themeContext from '../config/themeContext';
 import {ScrollView} from 'react-native-gesture-handler';
 import PushNotification from 'react-native-push-notification';
@@ -24,26 +19,15 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import AddTaskPage from '../components/AddTaskPage';
 import EditTaskPage from '../components/EditTaskPage';
+import styles from './component.style.js';
 
-const {width} = Dimensions.get('window');
 
 export default function TaskPage({navigation}) {
   // This is to manage Modal State
-  // Variable to modal add data
-  const [isModalVisible, setModalVisible] = useState(false);
+
 
   // Variable modal edit data
   const [isModalVisible1, setModalVisible1] = useState(false);
-
-  //category pop-up
-  const [chooseData, setchooseData] = useState('SELECT CATEGORY...');
-  const [isModalVisible_d, setisModalVisible_d] = useState(false); //
-  const changeModalVisibility = bool => {
-    setisModalVisible_d(bool);
-  };
-  const setData = option_drop => {
-    setchooseData(option_drop);
-  };
 
   // For loop data from firebase
   const [isLoading, setisLoading] = useState(false);
@@ -51,15 +35,14 @@ export default function TaskPage({navigation}) {
 
   // This is to manage TextInput State
   const theme = useContext(themeContext);
-  const [topic, topicInput] = useState('');
-  const [detailTask, detailTaskInput] = useState('');
+
 
   // This is user data from firebase
   const {user, logout} = useContext(AuthContext);
 
   // Variable contain value from user
 
-  const [docID, setDocId] = useState('');
+
   const [urlUser, setUrl] = useState('');
 
   // Variable to manage due date
@@ -67,8 +50,7 @@ export default function TaskPage({navigation}) {
   const [textDate, setText] = useState('CHOOSE DUE DATE...');
   const [textTime, setTime] = useState('CHOOSE DUE TIME...');
 
-  // Priority value
-  const [selectedValue, setSelectedValue] = useState('0');
+
 
   const [isModalVisible3, setModalVisible3] = useState(false);
 
@@ -137,135 +119,8 @@ export default function TaskPage({navigation}) {
     });
   };
 
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setShow(Platform.OS === 'ios');
-    setDate(currentDate);
-
-    let tempDate = new Date(currentDate);
-    let fDate =
-      tempDate.getDate() +
-      '/' +
-      (tempDate.getMonth() + 1) +
-      '/' +
-      tempDate.getFullYear();
-    let fTime =
-      'Hours: ' + tempDate.getHours() + ' Minutes:' + tempDate.getMinutes();
-    setText(fDate);
-    setTime(fTime);
-  };
-
-  const showMode = currentMode => {
-    setShow(true);
-    setMode(currentMode);
-  };
-
-  const showDatepicker = () => {
-    showMode('date');
-  };
-
-  const showTimepicker = () => {
-    showMode('time');
-  };
-
   //// Notification close /////
 
-  // Select image and get url firebase storage //
-  const selectImage = () => {
-    const options = {
-      maxWidth: 2000,
-      maxHeight: 2000,
-      storageOptions: {
-        skipBackup: true,
-        path: 'images',
-      },
-    };
-    launchImageLibrary(options, response => {
-      // Use launchImageLibrary to open image gallery
-      //console.log('Response = ', response.assets[0].uri);
-
-      if (response.didCancel) {
-        // console.log('User cancelled image picker');
-      } else if (response.error) {
-        // console.log('ImagePicker Error: ', response.error);
-      } else if (response.customButton) {
-        //  console.log('User tapped custom button: ', response.customButton);
-      } else {
-        const source = {uri: response.uri};
-
-        // You can also display the image using data:
-        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
-
-        const uri = response.assets[0].uri;
-        const filename = uri.substring(uri.lastIndexOf('/') + 1);
-        const uploadUri =
-          Platform.OS === 'android' ? uri.replace('file://', '') : uri;
-        const placeUrl = user.uid + '/' + 'task' + '/' + filename;
-        //  console.log(placeUrl);
-
-        setUploading(true);
-        setTransferred(0);
-        const task = storage().ref(placeUrl).putFile(uploadUri);
-        // set progress state
-
-        task.on('state_changed', snapshot => {
-          setTransferred(
-            Math.round(snapshot.bytesTransferred / snapshot.totalBytes) * 10000,
-          );
-          task.snapshot.ref.getDownloadURL().then(downloadURL => {
-            //  console.log('File available at', downloadURL);
-            setUrl(downloadURL);
-            //   console.log('Checkkkk  ', downloadURL);
-          });
-        });
-        setUploading(false);
-        Alert.alert('Photo uploaded!', 'Your photo has been uploaded');
-        setImage(null);
-        setUrl('');
-      }
-    });
-  };
-
-  // Function call to update task list
-  const updateTasklist = userDocId => {
-    setModalVisible1(!isModalVisible1);
-    // Set doc id
-    setDocId(userDocId);
-    // Call firebase to update
-    if (topic != '' && detailTask != '') {
-      const userCollection1 = firestore()
-        .collection('user')
-        .doc(user.uid)
-        .collection('Task')
-        .doc(docID);
-      // set new data
-      userCollection1.set({
-        timestamp: firestore.FieldValue.serverTimestamp(),
-        topic: topic,
-        taskDetail: detailTask,
-        urlPhoto: urlUser,
-        date: date,
-        textDate: textDate,
-        textTime: textTime,
-        priority: selectedValue,
-      });
-      // Set data to null
-      topicInput('');
-      detailTaskInput('');
-      setDataTask(dataTask);
-      setDocId('');
-      setUrl('');
-      setSelectedValue('0');
-    }
-  };
-
-  const cancelEdit = () => {
-    setModalVisible1(!isModalVisible1);
-  };
-
-  const cancelAdd = () => {
-    setModalVisible(!isModalVisible);
-  };
 
   // Function call to open modal edit
   const toggleModalVisibility1 = userDocId => {
@@ -502,328 +357,3 @@ export default function TaskPage({navigation}) {
     </SafeAreaView>
   );
 }
-
-// These are user defined styles
-const styles = StyleSheet.create({
-  notiShowTaskContainer: {
-    width: '100%',
-    height: 30,
-    backgroundColor: '#E5F1F1',
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    elevation: 10,
-    flexDirection: 'row',
-  },
-  taskdetailShowContainer: {
-    margin: 12,
-    marginVertical: 20,
-  },
-  textdetailShowTask: {
-    color: 'grey',
-    fontWeight: 'bold',
-    fontSize: 15,
-  },
-  headerShowTaskContainer: {
-    flex: 1,
-  },
-  textShowTask: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#000',
-  },
-  closeDetailContainer: {
-    backgroundColor: 'red',
-    width: 30,
-    height: 30,
-    borderWidth: 2,
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  showDetailTaskBody: {
-    flex: 1,
-    flexDirection: 'column',
-    alignContent: 'flex-start',
-  },
-  addButtonIcon: {
-    marginHorizontal: 7,
-    padding: 5,
-    borderRadius: 10,
-  },
-  buttonContainerIcon: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '35%',
-  },
-  taskText: {
-    fontWeight: 'bold',
-    fontSize: 18,
-    marginLeft: 10,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '35%',
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderColor: '#707070',
-    backgroundColor: '#FFFFFF',
-    padding: 10,
-  },
-  container: {
-    paddingTop: 50,
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
-  logo: {
-    width: 30,
-    height: 30,
-  },
-  logoutText: {
-    padding: 10,
-    fontSize: 24,
-  },
-  logoutContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logoutButton: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 25,
-    backgroundColor: 'tomato',
-    borderRadius: 10,
-  },
-  body: {
-    justifyContent: 'center',
-    flexDirection: 'column',
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    // backgroundColor: '#FFFFFF',
-  },
-  addButtonContainer: {
-    // backgroundColor: '#25ced1',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 75,
-    marginTop: 25,
-  },
-  addButtonText: {
-    // color: '#707070',
-    // fontWeight: 'bold',
-    fontSize: 50,
-  },
-  addButton: {
-    // backgroundColor: '#FFFFFF',
-    width: 70,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 100,
-    height: 70,
-    shadowColor: '#000000',
-    shadowOpacity: 5,
-    shadowRadius: 5,
-    elevation: 5,
-  },
-  header_container: {
-    marginLeft: 25,
-  },
-  textHeader: {
-    color: '#FFFFFF',
-    fontWeight: 'bold',
-    fontSize: 24,
-  },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    width: '100%',
-    height: 75,
-  },
-  screen: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-  },
-  viewWrapper: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.2)',
-  },
-  modalView: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    elevation: 5,
-    transform: [{translateX: -(width * 0.4)}, {translateY: -90}],
-    height: 400,
-    width: width * 0.8,
-    backgroundColor: '#fff',
-    borderRadius: 7,
-  },
-  textInput: {
-    width: '80%',
-    borderRadius: 5,
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    borderColor: 'rgba(0, 0, 0, 0.2)',
-    borderWidth: 1,
-    marginBottom: 8,
-  },
-  selectButton: {
-    borderRadius: 5,
-    width: 150,
-    height: 50,
-    backgroundColor: '#8ac6d1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  uploadButton: {
-    borderRadius: 5,
-    width: 150,
-    height: 50,
-    backgroundColor: '#ffb6b9',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 20,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  imageContainer: {
-    marginTop: 30,
-    marginBottom: 50,
-    alignItems: 'center',
-  },
-  progressBarContainer: {
-    marginTop: 20,
-  },
-  imageBox: {
-    width: 300,
-    height: 300,
-  },
-
-  bg_modal: {
-    backgroundColor: '#000000aa',
-    flex: 1,
-  },
-  paper_madal: {
-    backgroundColor: '#ffffff',
-    margin: 30,
-    marginTop: 90,
-    marginBottom: 90,
-    padding: 20,
-    borderRadius: 10,
-    flex: 1,
-    borderWidth: 15,
-    borderRadius: 15,
-    borderColor: '#25ced1',
-  },
-  text_normal: {
-    fontWeight: 'bold',
-    padding: 10,
-  },
-  input: {
-    width: '90%',
-    borderWidth: 1,
-    padding: 10,
-    fontSize: 16,
-    shadowColor: '#000000',
-    shadowOpacity: 5,
-    shadowRadius: 5,
-    elevation: 2,
-    backgroundColor: '#e5f1f1',
-    borderRadius: 5,
-  },
-  input2: {
-    width: '90%',
-    borderWidth: 1,
-    padding: 20,
-    fontSize: 16,
-    shadowColor: '#000000',
-    shadowOpacity: 5,
-    shadowRadius: 5,
-    elevation: 2,
-    backgroundColor: '#e5f1f1',
-    borderRadius: 5,
-    textAlignVertical: 'top',
-  },
-  style_text_date: {
-    fontSize: 16,
-    alignItems: 'center',
-    paddingTop: 4,
-    paddingLeft: 20,
-  },
-  input_f: {
-    width: '90%',
-    borderWidth: 1,
-    padding: 10,
-    fontSize: 16,
-    shadowColor: '#000000',
-    shadowOpacity: 5,
-    shadowRadius: 5,
-    elevation: 2,
-    backgroundColor: '#e5f1f1',
-    borderRadius: 5,
-    flexDirection: 'row',
-  },
-  logoPic: {
-    width: 250,
-    height: 250,
-  },
-  addButtonL: {
-    backgroundColor: '#707070',
-    width: '45%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
-    height: 40,
-    shadowColor: '#000000',
-    shadowOpacity: 5,
-    shadowRadius: 5,
-    elevation: 5,
-    marginBottom: 20,
-  },
-  addButtonR: {
-    backgroundColor: '#25ced1',
-    width: '45%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
-    height: 40,
-    shadowColor: '#000000',
-    shadowOpacity: 5,
-    shadowRadius: 5,
-    elevation: 5,
-    marginBottom: 20,
-  },
-  style_flex_button: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingTop: 1,
-    paddingBottom: 10,
-    padding: 20,
-  },
-  addButtonText1: {
-    color: '#ffffff',
-    fontWeight: 'bold',
-    fontSize: 16,
-    textAlign: 'center',
-  },
-});
