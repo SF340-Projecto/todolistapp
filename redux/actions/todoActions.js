f58af371016475f4e32c0a2a20a8193bc121347d
poom-cyber <poompim083@gmail.com>
2022-04-22 18:27:32 +0700

    Commit change state

--- redux/actions/todoActions.js
+++ redux/actions/todoActions.js
@@ -1,10 +1,18 @@
-import {API_TODO, API_TODODELETE, API_GETTASK, API_ADDTASK, API_UPDATETASK, API_DELETETASK, API_ACHIVETASK} from '../types';
+import {
+  API_TODO,
+  API_TODODELETE,
+  API_GETTASK,
+  API_ADDTASK,
+  API_UPDATETASK,
+  API_DELETETASK,
+  API_ACHIVETASK,
+} from '../types';
 import axios from 'axios';
 
 // const API_URL = 'https://app-todolist-api.herokuapp.com/todos';
 const API_URL = 'http://10.0.2.2:4001/todos';
 
-export const getTaskList = (user_id) => dispatch => {
+export const getTaskList = user_id => dispatch => {
   axios
     .get(API_URL + '/' + user_id)
     .then(response => {
@@ -18,7 +26,7 @@ export const getTaskList = (user_id) => dispatch => {
 };
 
 export const addTaskList =
-   (
+  (
     user_id,
     date,
     priority,
@@ -28,13 +36,13 @@ export const addTaskList =
     timestamp,
     topic,
     urlPhoto,
-    noti_id
+    noti_id,
   ) =>
-  (dispatch) => {
-    console.log(noti_id)
+  dispatch => {
+    console.log(noti_id);
     axios
       .post(API_URL, {
-        _id:user_id,
+        _id: user_id,
         date,
         priority,
         taskDetail,
@@ -42,17 +50,17 @@ export const addTaskList =
         taskDatetaskDate,
         timestamp,
         topic,
-        urlPhoto:urlPhoto,
-        noti_id
+        urlPhoto: urlPhoto,
+        noti_id,
       })
       .then(response => {
         //dispatch({type: API_TODOUPDATE, payload: response.data});
-        dispatch({type: API_ADDTASK, payload: []})
+        dispatch({type: API_ADDTASK, payload: []});
 
         return response.data;
       })
       .catch(err => {
-        console.log("Add fail")
+        console.log('Add fail');
       });
   };
 
@@ -69,7 +77,6 @@ export const updateTaskList =
     urlPhoto,
   ) =>
   dispatch => {
-      
     axios
       .put(API_URL, {
         _id,
@@ -80,43 +87,39 @@ export const updateTaskList =
         taskDatetaskDate,
         timestamp,
         topic,
-        urlPhoto:urlPhoto,
+        urlPhoto: urlPhoto,
       })
       .then(response => {
-        dispatch({type: API_UPDATETASK, payload: []})
+        dispatch({type: API_UPDATETASK, payload: []});
         return response.data;
       })
       .catch(err => {
-        console.log("Update fail")
-        console.log(err)
+        console.log('Update fail');
+        console.log(err);
       });
   };
 
-  export const achiveTask = _id => dispatch => {
-    axios
-      .put(API_URL, {
-        _id,
-        achive: true
-      })
-      .then(response => {
-        dispatch({type: API_ACHIVETASK, payload: []})
-        return response.data;
-      })
-      .catch(err => {
-        console.log("Update fail")
-        console.log(err)
-      });
-  };
-
+export const achiveTask = _id => dispatch => {
+  axios
+    .put(API_URL, {
+      _id,
+      achive: true,
+    })
+    .then(response => {
+      dispatch({type: API_ACHIVETASK, payload: []});
+      return response.data;
+    })
+    .catch(err => {
+      console.log('Update fail');
+      console.log(err);
+    });
+};
 
 export const deleteTask = _id => dispatch => {
   axios
     .delete(API_URL + '/' + _id)
-    .then( 
-      dispatch({type: API_DELETETASK, payload: []})
-    )
+    .then(dispatch({type: API_DELETETASK, payload: []}))
     .catch(err => {
-      console.log("Delete fail")
-
+      console.log('Delete fail');
     });
 };
